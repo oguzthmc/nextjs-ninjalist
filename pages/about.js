7c9e75703f19fd04e39a14d1541a1f7326131595
oguzthmc <47873038+oguzthmc@users.noreply.github.com>
2021-07-04 17:58:21 +0300

    images & metadata

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,19 @@
+import Head from 'next/head'
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi aspernatur velit? Animi, unde. Cum, est officiis. Labore eius maxime dolor, rerum odit perspiciatis? Modi, molestiae dicta. Omnis, nisi natus!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit vel. Quas error repudiandae ullam laboriosam? Optio consequatur ullam repellat ipsum est. Eveniet alias iste natus earum officiis iusto! Consequuntur totam esse, ea doloribus vero similique quibusdam. Harum quaerat alias inventore facere accusantium! Molestias, magni sed. Praesentium cupiditate aperiam odit!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos soluta, molestiae deleniti a nobis magni iusto vero minima eos.</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi aspernatur velit? Animi, unde. Cum, est officiis. Labore eius maxime dolor, rerum odit perspiciatis? Modi, molestiae dicta. Omnis, nisi natus!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit vel. Quas error repudiandae ullam laboriosam? Optio consequatur ullam repellat ipsum est. Eveniet alias iste natus earum officiis iusto! Consequuntur totam esse, ea doloribus vero similique quibusdam. Harum quaerat alias inventore facere accusantium! Molestias, magni sed. Praesentium cupiditate aperiam odit!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos soluta, molestiae deleniti a nobis magni iusto vero minima eos.</p>
+            </div>
+        </>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi aspernatur velit? Animi, unde. Cum, est officiis. Labore eius maxime dolor, rerum odit perspiciatis? Modi, molestiae dicta. Omnis, nisi natus!</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit vel. Quas error repudiandae ullam laboriosam? Optio consequatur ullam repellat ipsum est. Eveniet alias iste natus earum officiis iusto! Consequuntur totam esse, ea doloribus vero similique quibusdam. Harum quaerat alias inventore facere accusantium! Molestias, magni sed. Praesentium cupiditate aperiam odit!</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos soluta, molestiae deleniti a nobis magni iusto vero minima eos.</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi aspernatur velit? Animi, unde. Cum, est officiis. Labore eius maxime dolor, rerum odit perspiciatis? Modi, molestiae dicta. Omnis, nisi natus!</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit vel. Quas error repudiandae ullam laboriosam? Optio consequatur ullam repellat ipsum est. Eveniet alias iste natus earum officiis iusto! Consequuntur totam esse, ea doloribus vero similique quibusdam. Harum quaerat alias inventore facere accusantium! Molestias, magni sed. Praesentium cupiditate aperiam odit!</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos soluta, molestiae deleniti a nobis magni iusto vero minima eos.</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
