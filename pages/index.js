6e995e62ea8c024d700456c34669ee3ddfbf94d8
oguzthmc <47873038+oguzthmc@users.noreply.github.com>
2021-07-04 13:14:02 +0300

    creating a layout component

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi aspernatur velit? Animi, unde. Cum, est officiis. Labore eius maxime dolor, rerum odit perspiciatis? Modi, molestiae dicta. Omnis, nisi natus!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit vel. Quas error repudiandae ullam laboriosam? Optio consequatur ullam repellat ipsum est. Eveniet alias iste natus earum officiis iusto! Consequuntur totam esse, ea doloribus vero similique quibusdam. Harum quaerat alias inventore facere accusantium! Molestias, magni sed. Praesentium cupiditate aperiam odit!</p>
@@ -15,7 +14,6 @@ export default function Home() {
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
