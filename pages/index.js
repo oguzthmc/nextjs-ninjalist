7b453a2fe2f24ff73e56977cd23cada395c2e2c2
oguzthmc <47873038+oguzthmc@users.noreply.github.com>
2021-07-04 10:27:15 +0300

    pages & routes

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi aspernatur velit? Animi, unde. Cum, est officiis. Labore eius maxime dolor, rerum odit perspiciatis? Modi, molestiae dicta. Omnis, nisi natus!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit vel. Quas error repudiandae ullam laboriosam? Optio consequatur ullam repellat ipsum est. Eveniet alias iste natus earum officiis iusto! Consequuntur totam esse, ea doloribus vero similique quibusdam. Harum quaerat alias inventore facere accusantium! Molestias, magni sed. Praesentium cupiditate aperiam odit!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos soluta, molestiae deleniti a nobis magni iusto vero minima eos.</p>
     </div>
   )
 }
