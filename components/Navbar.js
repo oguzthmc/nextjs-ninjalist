710eec2e6bc0094c1545601a1c53804ad2cc88c9
oguzthmc <47873038+oguzthmc@users.noreply.github.com>
2021-07-04 18:47:52 +0300

    dynamic routes (part 1)

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,14 +4,16 @@ import Image from 'next/image'
 const Navbar = () => {
     return (
         <nav>
-            <div className="logo">
-                <Image
-                    src="/logo.png"
-                    alt="Ninja Logo"
-                    width={128}
-                    height={77}
-                />
-            </div>
+            <Link href="/">
+                <div className="logo">
+                    <Image
+                        src="/logo.png"
+                        alt="Ninja Logo"
+                        width={128}
+                        height={77}
+                    />
+                </div>
+            </Link>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
             <Link href="/ninjas"><a>Ninja Listing</a></Link>
